9479540f2500b365dba4f7dbfc4116ed33142806
saopeter <107004118+saopeter@users.noreply.github.com>
2022-08-01 13:19:48 -0400

    Add files via upload

--- numpyro_sampler.py
+++ numpyro_sampler.py
@@ -10,9 +10,19 @@ import jax
 from jax import random
 
 from m87_calibration import calibrate
+from sampling_utils import *
 
 eht = eh.array.load_txt('EHT2019.txt')
+
+# Fitting parameters
+priors = {"F0":(0, 10), "d": (5, 60), "w": (10, 30), "betas": 1} # F0, d, w uniform; betas is a number of parameters, each of which is uniform in (0,1) and (-pi to pi) in phase
+fit = {"visibilities": False, "visibility amplitudes": False, "closure phases": True, "log closure amplitudes": True}
 fit_target = "simulated" #"M87" or "simulated"
+
+#NUTS hyperparameters
+num_warmup = 2000
+num_samples = 4000
+run_NUTS = True
 
 # # Circular Gaussian model
 # #units of microarcseconds
@@ -25,7 +35,6 @@ fit_target = "simulated" #"M87" or "simulated"
 
 # print(f'underlying model parameters: dx:{dx:.2e}, dy:{dy:.2e}, h:{h:.2e}, f:{f:.2e}\n')
 
-# #eht-imaging likes units of radians, so we convert all our non-flux parameters
 # model_type = "circ_gauss"
 # model = model.add_circ_gauss(f, h*eh.RADPERUAS, x0=dx*eh.RADPERUAS, y0=dy*eh.RADPERUAS)
 
@@ -39,15 +48,12 @@ w = 20
 x0 = 0
 y0 = 0
 #dimensionless (complex):
-betas_amplitudes = [.5]
-betas_phases = [jnp.pi]
+# betas_amplitudes = [.1]
+betas_amplitudes = [.1, .05]
+# betas_phases = [jnp.pi/4]
+betas_phases = [jnp.pi/4, 3*jnp.pi/4]
 
 params = {"F0":F0, "d":d, "w":w, "x0":x0, "y0":y0, "betas_amplitudes":betas_amplitudes, "betas_phases":betas_phases}
-
-
-# Fitting parameters
-priors = {"F0":(0, 10), "d": (5, 60), "w": (10, 30), "betas": 1} # F0, d, w uniform; betas is a number of parameters, each of which is uniform in (0,1) and (-pi to pi) in phase
-fit = {"visibilities": False, "visibility amplitudes": True, "closure phases": True, "log closure amplitudes": False}
 
 # Helper function for converting between ehtim mring and my mring model parameters
 def convert_params_to_ehtim(params, model_type=model_type):
@@ -61,8 +67,8 @@ def convert_params_to_ehtim(params, model_type=model_type):
         ehtim_params['F0'] = params['F0']
         ehtim_params['d'] = params['d']*eh.RADPERUAS
         ehtim_params['alpha'] = params['w']*eh.RADPERUAS
-        ehtim_params['x0'] = 0
-        ehtim_params['y0'] = 0
+        ehtim_params['x0'] = params['x0']*eh.RADPERUAS
+        ehtim_params['y0'] = params['y0']*eh.RADPERUAS
         betas = [ betas_amplitudes[i]*jnp.exp((1j*betas_phases[i])) for i in range(len(betas_amplitudes)) ]
         ehtim_params['beta_list'] = betas
 
@@ -111,9 +117,9 @@ elif fit_target == "simulated":
     model = model.add_thick_mring(**ehtim_params)
 
     # obs = model.observe(eht, tint_sec, tadv_sec, tstart_hr, tstop_hr, bw_hz, ampcal=True, phasecal=True,seed=4)
-    obs = model.observe(eht, tint_sec, tadv_sec, tstart_hr, tstop_hr, bw_hz, ampcal=True, phasecal=False,seed=7)
+    obs = model.observe(eht, tint_sec, tadv_sec, tstart_hr, tstop_hr, bw_hz, ampcal=True, phasecal=False,seed=9)
     obs = obs.add_fractional_noise(.1)
-    obs = model.observe_same_nonoise(obs)
+    # obs = model.observe_same_nonoise(obs)
 
 def generate_circ_gauss_model(dx, dy, h, f):
     # re-convert from microarcseconds back to radians
@@ -121,8 +127,8 @@ def generate_circ_gauss_model(dx, dy, h, f):
     dy = dy*eh.RADPERUAS
     h = h*eh.RADPERUAS
     def forward_model(u, v):
-        return f * jnp.exp(-jnp.pi**2/(4.*jnp.log(2.)) * (u**2 + v**2) * h**2)\
-               * jnp.exp(1j * 2.0 * jnp.pi * (u * dx + v * dy))
+        return (f * jnp.exp(-jnp.pi**2/(4.*jnp.log(2.)) * (u**2 + v**2) * h**2)
+                  * jnp.exp(1j * 2.0 * jnp.pi * (u * dx + v * dy)))
     return forward_model
 
 # replacement for scipy.special.jv, which is not available in jax
@@ -148,37 +154,63 @@ def bessel_j(n, z, num_samples=100):
 # 1, 1 betas eg.1 (note: this produces negative images)
 # 1, 1/root2 1/root2i eg. 2
 
-# TODO: add x0 and y0 parameters
 def generate_m_ring_model(F0, d, w, betas_amplitudes, betas_phases, x0=0, y0=0):
     d = d*eh.RADPERUAS
     w = w*eh.RADPERUAS
+    x0 = x0*eh.RADPERUAS
+    y0 = y0*eh.RADPERUAS
     betas = jnp.asarray( [ betas_amplitudes[i]*jnp.exp((1j*betas_phases[i])) for i in range(len(betas_amplitudes)) ])
     def forward_model(u, v):
         mag = jnp.sqrt(u**2+ v**2)
         phi = jnp.arctan2(u,v)
-        # phi += jnp.pi
+        phi += jnp.pi # This sign flip is needed to match eht-imaging conventions
         extended_betas = jnp.concatenate( (jnp.flip(jnp.conj(betas)), jnp.array([1]), betas )) #[complex conjugates of betas, 1, betas]
         ks = jnp.arange(-len(betas), len(betas)+1)
         exps = jnp.array([jnp.exp(1j*n*(phi-jnp.pi/2.)) for n in ks]).T
-        # exps = jnp.array([jnp.exp(1j*n*(phi)) for n in ks]).T
         Js = jnp.array([bessel_j(n, jnp.pi*mag*d) for n in ks]).T
         V_ring = F0*jnp.sum(extended_betas*Js*exps, axis=1)
         V_ring = jnp.exp((-jnp.pi**2*w**2*mag**2)/(4.*jnp.log(2.)))*V_ring
-        return jnp.conj(V_ring)
+        V_ring = V_ring * jnp.exp(1j * 2.0 * jnp.pi * (u * x0 + v * y0))
+        return V_ring
     return forward_model
 
-# def generate_m_ring_model_test(F0, d, w, betas_amplitudes, betas_phases, x0=0, y0=0):
-#         phi = jnp.angle(v + 1j*u)
-#         # Flip the baseline sign to match eht-imaging conventions
-#         phi += np.pi
-#         z = np.pi * params['d'] * (u**2 + v**2)**0.5
-#         beta_factor = (sps.jv(0, z) 
-#             + np.sum([params['beta_list'][m-1]          * sps.jv( m, z) * np.exp( 1j * m * (phi - np.pi/2.)) for m in range(1,len(params['beta_list'])+1)],axis=0)
-#             + np.sum([np.conj(params['beta_list'][m-1]) * sps.jv(-m, z) * np.exp(-1j * m * (phi - np.pi/2.)) for m in range(1,len(params['beta_list'])+1)],axis=0))
+def calc_closure_phases(ft, cphase_data):
+    # parameters: ft is a function that returns the Fourier transform at coordinates u and v
+    #             cphase_data is the obsdata closure phases object
+    cphases_u1 = cphase_data['u1']
+    cphases_u2 = cphase_data['u2']
+    cphases_u3 = cphase_data['u3']
+    cphases_v1 = cphase_data['v1']
+    cphases_v2 = cphase_data['v2']
+    cphases_v3 = cphase_data['v3']
 
-#         val = (params['F0'] * beta_factor
-#                * np.exp(-(np.pi * params['alpha'] * (u**2 + v**2)**0.5)**2/(4. * np.log(2.)))
-#                * np.exp(1j * 2.0 * np.pi * (u * params['x0'] + v * params['y0']))) 
+    cphase12 = jnp.angle(ft(cphases_u1, cphases_v1))
+    cphase23 = jnp.angle(ft(cphases_u2, cphases_v2))
+    cphase31 = jnp.angle(ft(cphases_u3, cphases_v3))
+
+    model_cphase = cphase12 + cphase23 + cphase31
+    reparameterized_cphase = jnp.remainder(model_cphase + jnp.pi, 2*jnp.pi) - jnp.pi
+    return reparameterized_cphase
+
+def calc_log_closure_amplitudes(ft, logcamp):
+    # parameters: ft is a function that returns the Fourier transform at coordinates u and v
+    #             logcamp is the obsdata log closure amplitudes object
+    lcamps_u1 = logcamp['u1']
+    lcamps_u2 = logcamp['u2']
+    lcamps_u3 = logcamp['u3']
+    lcamps_u4 = logcamp['u4']
+    lcamps_v1 = logcamp['v1']
+    lcamps_v2 = logcamp['v2']
+    lcamps_v3 = logcamp['v3']
+    lcamps_v4 = logcamp['v4']
+
+    lcamp12 = jnp.log(jnp.abs(ft(lcamps_u1, lcamps_v1)))
+    lcamp34 = jnp.log(jnp.abs(ft(lcamps_u2, lcamps_v2)))
+    lcamp23 = jnp.log(jnp.abs(ft(lcamps_u3, lcamps_v3)))
+    lcamp14 = jnp.log(jnp.abs(ft(lcamps_u4, lcamps_v4)))
+
+    model_lcamp = lcamp12 + lcamp34 - lcamp23 - lcamp14
+    return model_lcamp
 
 def gaussian_model():
     dx = numpyro.sample("dx", dist.Uniform(low=-40, high=40))
@@ -196,10 +228,8 @@ def gaussian_model():
     numpyro.sample("re(obs)", dist.Normal(ft(u,v).real, sigma), obs = vis.real)
     numpyro.sample("im(obs)", dist.Normal(ft(u,v).imag, sigma), obs = vis.imag)
 
-# @numpyro.handlers.reparam(config={"closure phases": numpyro.infer.reparam.CircularReparam()})
 def mring_model(obs=obs, priors=priors, fit=fit):
 
-    # parameters: F0, d, w, betas
     # distributions are uniform; betas are specified as a number
 
     F0 = numpyro.sample("F0", dist.Uniform(low=priors["F0"][0], high=priors["F0"][1]))
@@ -210,9 +240,10 @@ def mring_model(obs=obs, priors=priors, fit=fit):
     # TODO: use numpyro plate notation? 
     for i in range(priors["betas"]):
         beta_amp = numpyro.sample("abs(b"+str(i+1)+")", dist.Uniform(low=0, high=1))
-        #TODO: encode circularity of phases for better sampling of higher order beta parameters
-        beta_phase = numpyro.sample("angle(b"+str(i+1)+")", dist.Uniform(low=0, high=2*jnp.pi))
-        # beta_phase = numpyro.sample("angle(b"+str(i+1)+")", dist.VonMises(0, 0))
+        # beta_phase = numpyro.sample("angle(b"+str(i+1)+")", dist.Uniform(low=0, high=2*jnp.pi))
+        # NOTE: something about this looks and feels kind of wonky
+        with numpyro.handlers.reparam(config= {"angle(b"+str(i+1)+")": numpyro.infer.reparam.CircularReparam()} ):
+            beta_phase = numpyro.sample("angle(b"+str(i+1)+")", dist.VonMises(0, 0.001)) #set concentration to something approx. ~0 to simulate a uniform distribution, but not exactly 0 to avoid calculation errors
         betas_amplitudes.append(beta_amp)
         betas_phases.append(beta_phase)
 
@@ -234,53 +265,18 @@ def mring_model(obs=obs, priors=priors, fit=fit):
         numpyro.sample("visibility amplitudes", dist.Normal(abs(ft(u,v)), vis_sigma), obs = vis_amp)
 
     if fit["closure phases"]:
-        # TODO: refactor these sections, this code is going to be similar across all models
-        #Pull out closure phases from simulated data
         cphase_data = obs.c_phases(ang_unit='rad')
-        cphases = cphase_data['cphase']
+        observed_cphases = cphase_data['cphase']
         cphases_sigmas = cphase_data['sigmacp']
-
-        # Calculate our model closure phases
-        cphases_u1 = cphase_data['u1']
-        cphases_u2 = cphase_data['u2']
-        cphases_u3 = cphase_data['u3']
-        cphases_v1 = cphase_data['v1']
-        cphases_v2 = cphase_data['v2']
-        cphases_v3 = cphase_data['v3']
-
-        cphase12 = jnp.angle(ft(cphases_u1, cphases_v1))
-        cphase23 = jnp.angle(ft(cphases_u2, cphases_v2))
-        cphase31 = jnp.angle(ft(cphases_u3, cphases_v3))
-
-        model_cphase = cphase12 + cphase23 + cphase31
-        reparameterized_cphase = jnp.remainder(model_cphase + jnp.pi, 2*jnp.pi) - jnp.pi
-        numpyro.sample("closure phases", dist.VonMises(reparameterized_cphase, 1/cphases_sigmas**2), obs = cphases)
+        model_cphases = calc_closure_phases(ft, cphase_data)
+        numpyro.sample("closure phases", dist.VonMises(model_cphases, 1/cphases_sigmas**2), obs = observed_cphases)
 
     if fit["log closure amplitudes"]:
-        # Pull out closure amplitudes from simulated data
         logcamp = obs.c_amplitudes(ctype='logcamp', debias=True)
-        lcamps = logcamp['camp']
+        observed_logcamps = logcamp['camp']
         lcamps_sigmas = logcamp['sigmaca']
-
-        # Calculate our model log closure amplitudes
-        lcamps_u1 = logcamp['u1']
-        lcamps_u2 = logcamp['u2']
-        lcamps_u3 = logcamp['u3']
-        lcamps_u4 = logcamp['u4']
-        lcamps_v1 = logcamp['v1']
-        lcamps_v2 = logcamp['v2']
-        lcamps_v3 = logcamp['v3']
-        lcamps_v4 = logcamp['v4']
-
-        lcamp12 = jnp.log(jnp.abs(ft(lcamps_u1, lcamps_v1)))
-        lcamp34 = jnp.log(jnp.abs(ft(lcamps_u2, lcamps_v2)))
-        lcamp23 = jnp.log(jnp.abs(ft(lcamps_u3, lcamps_v3)))
-        lcamp14 = jnp.log(jnp.abs(ft(lcamps_u4, lcamps_v4)))
-
-        # Fit closure amplitudes
-        model_lcamp = lcamp12 + lcamp34 - lcamp23 - lcamp14
-        # print("Fitting closure amplitudes")
-        numpyro.sample("log closure amplitudes", dist.Normal(model_lcamp, lcamps_sigmas), obs = lcamps)
+        model_logcamps = calc_log_closure_amplitudes(ft, logcamp)
+        numpyro.sample("log closure amplitudes", dist.Normal(model_logcamps, lcamps_sigmas), obs = observed_logcamps)
 
 PPL_model = None
 if model_type == "circ_gauss":
@@ -302,13 +298,12 @@ rng_key, rng_key_ = random.split(rng_key)
 
 # Run NUTS.
 kernel = NUTS(PPL_model)
-num_warmup = 1000
-num_samples = 2000
 mcmc = MCMC(kernel, num_warmup=num_warmup, num_samples=num_samples)
-mcmc.run(
-    rng_key_
-)
-mcmc.print_summary()
+if run_NUTS:
+    mcmc.run(
+        rng_key_
+    )
+    mcmc.print_summary()
 
 #Assorted helper functions
 
@@ -408,7 +403,61 @@ def get_last_sample(samples):
     output["betas_phases"] = betas_phases
     return output
 
-def plot_all(samples=mcmc.get_samples(), model_type=None, truths=None, save=False, filename=None):
+def plot_all(samples, model_type=None, truths=None, save=False, filename=None):
     plot_trace(samples, model_type=model_type, truths=truths, save=save, filename=filename+"_trace")
     plot_posterior(samples, model_type=model_type, truths=truths, save=save, filename=filename+"_posterior")
     params_to_image(get_last_sample(samples), save=save, filename=filename+"_image")
+
+#TODO: deduplicate code in this section
+def validation_mring_cphases(params=params, obs=obs):
+        # Returns the chi squared value for our calculated closure phases vs. the observed values provided by obs
+        #Pull out closure phases from simulated data
+        cphase_data = obs.c_phases(ang_unit='rad')
+        cphases = cphase_data['cphase']
+        cphases_sigmas = cphase_data['sigmacp']
+
+        ft = generate_m_ring_model(**params)
+        model_cphases = calc_closure_phases(ft, cphase_data)
+        circle_dists = jnp.array( [abs(model_cphases-2*jnp.pi - cphases),
+                                   abs(model_cphases - cphases),
+                                   abs(model_cphases+2*jnp.pi - cphases)] ) # TODO: make this less clunky
+        circle_dists = jnp.min(circle_dists, axis=0)
+        chi_sq = circle_dists**2/cphases_sigmas**2
+        return sum(chi_sq)/len(chi_sq)
+
+def validation_mring_logcamps(params=params, obs=obs):
+        # Returns the chi squared value for our calculated log closure amplitudes vs. the observed values provided by obs
+        # Pull out closure amplitudes from simulated data
+        logcamp = obs.c_amplitudes(ctype='logcamp', debias=True)
+        lcamps = logcamp['camp']
+        lcamps_sigmas = logcamp['sigmaca']
+
+        ft = generate_m_ring_model(**params)
+        model_lcamp = calc_log_closure_amplitudes(ft, logcamp)
+        chi_sq = (model_lcamp - lcamps)**2/lcamps_sigmas**2
+        return sum(chi_sq)/len(chi_sq)
+
+def validation_mring_vis_amps(params=params, obs=obs):
+    u = obs.data['u'] 
+    v = obs.data['v']
+    vis_amp = obs.unpack(['amp'],debias=True)['amp']
+    vis_sigma = obs.data['sigma']
+
+    ft = generate_m_ring_model(**params)
+    model_vis = abs(ft(u, v))
+    chi_sq = (model_vis - vis_amp)**2/vis_sigma**2
+    return sum(chi_sq)/len(chi_sq)
+
+def validation_mring_sample_uv(params=params,obs=obs):
+    # verify that for an mring model specified by params, sampling at obs data points u and v returns the same answer for ehtim's sample-uv and my own model's ft
+    model = eh.model.Model()
+    ehtim_params = convert_params_to_ehtim(params, model_type="mring")
+    model = model.add_thick_mring(**ehtim_params)
+    obs = model.observe_same_nonoise(obs)
+    u = obs.data['u']
+    v = obs.data['v']
+    y = model.sample_uv(u, v)
+    ft = generate_m_ring_model(**params)
+    yy = ft(u, v)
+    return jnp.allclose(y, yy)
+
